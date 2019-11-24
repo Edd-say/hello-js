fd0e2556ee3b90ba6cce84f6dbffb9feede9538c
Edd-say <eddisteem@gmail.com>
2019-11-24 15:53:48 +0200

    lessons 10

--- public/game2048.js
+++ public/game2048.js
@@ -1,9 +1,9 @@
 const boardSize = 4;
 
 var board = [
-     [ 0, 0, 0, 0 ],
-     [ 0, 2, 0, 0 ], 
-     [ 0, 0, 2, 2 ],
+     [ 0, 0, 2, 4 ],
+     [ 0, 0, 2, 0 ], 
+     [ 0, 2, 0, 2 ],
      [ 0, 2, 0, 0 ],
 ];
 
@@ -25,14 +25,49 @@ function drawboard(){
 }
 drawboard();
 
+function mover( a ){
 // [ 0, 0, 0, 0 ]
-// 1) всі клітинки без останньої -1
-// 2) 0 ? (ТАК)
-// 2.1) вправо ? > 0 (cNe0)
-// 2.2) ( c ) = ( cNe0) ; (cNe0) = 0 
-// 3) вправо = c ? так
-// 3.1)  ( c ) +=)(cEq); (cEq = 0)+  
-
-
-
-
+for ( let c=0; c < boardSize-1;c++) {                  // 1) всі клітинки без останньої -1
+    if ( a[ c ] == 0) {
+        let cNe0 = -1;                                      // 2) 0 ? (ТАК)
+    for (let cR=c+1; cR < boardSize; cR++ ) {
+    if (a [cR] > 0){                         // 2.1) вправо ? > 0 (cNe0)
+            cNe0 = cR;
+            break;
+        }
+    }
+    if (cNe0 != -1 ) {
+    a[ c ] = a [cNe0]
+    a[cNe0] = 0;             // 2.2) ( c ) = ( cNe0) ; (cNe0) = 0 
+        }   
+    }
+    let cEq = -1;
+    for (let cR=c+1; cR , boardSize; cR++ ){        // 3) вправо = c ? так
+        if (a [ c ] == a [ cR ] ){
+            cEq = cR;
+            break;
+        }
+    }
+    if ( cEq != -1){
+        a[ c ] += a [cEq];                  // 3.1)  ( c ) +=)(cEq); (cEq = 0)+
+        a[cEq] = 0; 
+    }
+}
+return a;
+}
+function mvleft(){
+    for (let r=0; r< boardSize; r++){
+       // let row = board [r];
+       // row = mover( row );
+       // board[r] = row;
+       board[r] = mover(board[r] );
+    }
+    drawboard();
+}
+ function mvRight() {
+    for (let r=0; r < boardSize; r++ ) {
+        board[r] = mover(board[r].reverse() ).reverse();
+    }
+ drawboard();
+}
+  
